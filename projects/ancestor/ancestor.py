ae38f70c99a94157f4ca8dabd5cc67add6bdd604
VeraMendes <veramendes10@gmail.com>
2020-04-14 23:21:34 -0700

    mvp day2

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,63 +1,52 @@
 # directed acyclic graph
 
-class Graph():
+class Queue():
     def __init__(self):
-        self.vertices = {}
-
-    def add_vertex(self, vertex_id):
-        """
-        Add a vertex to the graph.
-        """
-        self.vertices[vertex_id] = set()
-
-    def add_edge(self, v1, v2):
-        """
-        Add a directed edge to the graph.
-        Add a directed edge to the graph from v1 to v2
-        """
-        # Check if they exist
-        if v1 in self.vertices and v2 in self.vertices:
-            # Add the edge
-            self.vertices[v1].add(v2)
-        else:
-            print("ERROR ADDING EDGE: Vertex not found")
-
-    def get_neighbors(self, vertex_id):
-        """
-        Get all neighbors (edges) of a vertex.
-        """
-        if vertex_id in self.vertices:
-            return self.vertices[vertex_id]
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
         else:
             return None
+    def size(self):
+        return len(self.queue)
 
 
+def earliest_ancestor(ancestors, starting_node):
 
-def earliest_ancestor(ancestors, node):
+    # level in the graph
+    max_level = 0
+    # current min value (if node has no parents, min value is -1)
+    min_value = -1
 
-    # instantiate graph
-    graph = Graph()
-    # create graph
-    # add node to graph
-    # node = graph.add_vertex(node)
-    for pair in ancestors:
-        graph.add_vertex(pair[0])
-        graph.add_vertex(pair[1])
-        graph.add_edge(pair[1], pair[0])
-    # check if node has parents
-    # if yes with 1 parent
-    if len(graph.get_neighbors(node)) == 1:
-        parent = graph.get_neighbors(node)
-        earliest_ancestor(ancestors, parent)
-    # if yes with 2 parents
-    # choose the smallest parent id
-    elif len(graph.get_neighbors(node)) == 2:
-        smallest_id_parent = min(graph.get_neighbors(node))
-        earliest_ancestor(ancestors, smallest_id_parent)
-    # if not, we found the earliest ancestor
-    else:
-        return node
+    # instantiate a queue
+    q = Queue()
+    # enqueue tuple with the starting value and starting level
+    q.enqueue((starting_node, 0))
 
+    # process queue while queue is not empty
+    while q.size() > 0:
+        current = q.dequeue()
 
-    # If the input individual has no parents, returns -1
-    return -1
+        for tup in ancestors:
+            # check ancestors tup[1] for all relationships with current[0]
+            if tup[1] == current[0]:
+                next_level = current[1] + 1
+                # if neighbor is found
+                if next_level > max_level:
+                    # max level increments
+                    max_level = next_level
+                    # min value will be set to a new value from tup
+                    min_value = tup[0]
+                # otherwise compare neighbor at the same level, if lower than current min value
+                elif tup[0] < min_value:
+                    # min value only change if new value found is lower than min value in this level
+                    min_value = tup[0]
+                # enqueue the neighbor tup as tup[0] and next level
+                q.enqueue((tup[0], next_level))
+    
+    return min_value
+
+# ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
